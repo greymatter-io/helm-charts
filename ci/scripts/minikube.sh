e394b7dbd5c3b147551a99b55f19be22eeb834c8
nicholas.sorenson <nick.sorenson@deciphernow.com>
2019-11-27 15:12:29 -0500

    Better introspection and fix paths.

--- ci/scripts/minikube.sh
+++ ci/scripts/minikube.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cd $(dirname "${BASH_SOURCE[0]}")
+cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 #Determine if we are on AWS or not
 MINI='minikube'
-curl -m 2 169.254.169.254/latest/meta-data > /dev/null
-RC=$?
-if [ $RC -ne 0 ]; then
+LC=$(curl -s -m 2 169.254.169.254/latest/meta-data | wc -l )
+if [ $LC -ge 4 ]; then
     MINI='sudo /home/ubuntu/bin/minikube' 
 fi
 
 $MINI start --memory 6144 --cpus 6
+
+if [ $LC -ge 4 ]; then
+    sudo chown -R ubuntu /home/ubuntu/.kube /home/ubuntu/.minikube
+fi
+
 helm init --wait
 ./ci/scripts/install-voyager.sh
 helm install decipher/greymatter -f greymatter.yaml -f greymatter-secrets.yaml -f credentials.yaml --set global.environment=kubernetes --set global.k8s_use_voyager_ingress=true -n gm-deploy
--- ci/scripts/show-voyager.sh
+++ ci/scripts/show-voyager.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Grey Matter Dashboard is running at: https://$(minikube ip -p gm-deploy):30000"
+
+MINI='minikube'
+LC=$(curl -s -m 2 169.254.169.254/latest/meta-data | wc -l )
+if [ $LC -ge 4 ]; then
+    MINI='sudo /home/ubuntu/bin/minikube' 
+fi
+
+echo "Grey Matter Dashboard is running at: https://$($MINI ip):30000"
