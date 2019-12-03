11f751e618837a62154cd67afb2cf5779cb91a99
nicholas.sorenson <nick.sorenson@deciphernow.com>
2019-12-03 11:38:46 -0500

    Simplification and making easier to use with non-devinabox (how dare they) aws machines.

--- ci/scripts/minikube.sh
+++ ci/scripts/minikube.sh
@@ -3,14 +3,12 @@
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 #Determine if we are on AWS or not
-MINI='minikube'
 LC=$(curl -s -m 2 169.254.169.254/latest/meta-data | wc -l )
 if [ $LC -ge 4 ]; then
-    MINILOC=$(which minikube)
-    MINI="sudo $MINILOC" 
+    minikube config set vm-driver none
 fi
 
-$MINI start --memory 6144 --cpus 6
+minikube start --memory 6144 --cpus 6
 
 if [ $LC -ge 4 ]; then
     sudo chown -R ubuntu /home/ubuntu/.kube /home/ubuntu/.minikube
