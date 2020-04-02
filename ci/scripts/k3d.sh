d504959492c95631ce82800bbeb394911fefe3a0
moosecanswim <kyle.gallagher@deciphernow.com>
2020-04-02 09:39:55 -0400

    export every time cluster is connected

--- ci/scripts/k3d.sh
+++ ci/scripts/k3d.sh
@@ -6,4 +6,4 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | TAG=v1
 k3d create --workers 4 --name greymatter
 while [[ $(k3d get-kubeconfig --name='greymatter') != *kubeconfig.yaml ]]; do echo "echo waiting for k3d cluster to start up" && sleep 10; done
 export KUBECONFIG="$(k3d get-kubeconfig --name='greymatter')"
-echo "Cluster is up"
+echo "Cluster is connected"
