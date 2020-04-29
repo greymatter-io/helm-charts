ba51ba4abee901ffaa55321b628e6a49509d9390
dgoldstein1 <david.goldstein@deciphernow.com>
2020-04-29 09:19:56 -0400

    small edits

--- ci/scripts/k3d.sh
+++ ci/scripts/k3d.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# install k3d 1.6.0
+# install k3d 1.7.0
 curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | TAG=v1.7.0 bash
 
 k3d create --workers 4 --name greymatter --publish 30000:10808 
