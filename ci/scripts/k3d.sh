37d10c80765e043460d986b9768fc2402a094735
EddyIV <edokeefe4@gmail.com>
2020-07-30 12:38:18 -0400

    fix makefile output

--- ci/scripts/k3d.sh
+++ ci/scripts/k3d.sh
@@ -6,5 +6,5 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 k3d cluster delete greymatter
 
 k3d cluster create greymatter -a 4 --api-port 30000 && sleep 10
-export KUBECONFIG="$(k3d kubeconfig get greymatter)"
+export KUBECONFIG=$(k3d kubeconfig get greymatter)
 echo "Cluster is connected"
