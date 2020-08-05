38faa1fc3460304f9f96410e12bb789a7d090536
zoemccormick <zmm7@cornell.edu>
2020-08-05 17:11:31 -0400

    fix issue with quotes for acert command

--- ci/scripts/spire-ca.sh
+++ ci/scripts/spire-ca.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-addflg() {
-    flags=$1
-    flg=$2
-    flgval=$3
-    if ! [[ -z "$flgval" ]]; then
-        n="$flg $flgval "
-        flags+=$n
-    fi
-}
-
 cacfg() {
     echo Enter the SPIRE release namespace, or press enter to use the default = \"spire\":
     read NS
@@ -18,26 +8,24 @@ cacfg() {
 
     echo Enter the following to configure the authority, or press enter to use the default
     echo Common Name \(default Acert\):
-    read CN; addflg "$flags" "-n" "$CN"
+    read N; if [[ -z "$N" ]]; then N=Acert; fi
     echo Organization \(default Decipher Technology Studios\):
-    read ORG; addflg "$flags" "-o" "$ORG"
+    read O; if [[ -z "$O" ]]; then O=Decipher Technology Studios; fi
     echo Country \(default US\):
-    read CTRY; addflg "$flags" "-c" "$CTRY"
+    read C; if [[ -z "$C" ]]; then C=US; fi
     echo State \(default Virginia\):
-    read STATE; addflg "$flags" "-s" "$STATE"
+    read S; if [[ -z "$S" ]]; then S=Virginia; fi
     echo Locality \(default Alexandria\):
-    read LOC; addflg "$flags" "-l" "$LOC"
+    read L; if [[ -z "$L" ]]; then L=Alexandria; fi
     echo Organizational Unit \(default Engineering\):
-    read ORGU; addflg "$flags" "-u" "$ORGU"
+    read U; if [[ -z "$U" ]]; then U=Engineering; fi
     echo Expiration Time \(default 87600h0m0s\):
-    read EXP; addflg "$flags" "-e" "$EXP"
+    read E; if [[ -z "$E" ]]; then E=87600h0m0s; fi
     echo Street Address \(default none\):
-    read ADDR; addflg "$flags" "-a" "$ADDR"
+    read A;
     echo Postal Code \(default none\):
-    read ZIP; addflg "$flags" "-p" "$ZIP"
+    read P;
 }
-
-flags=""
 
 cd $(dirname "${BASH_SOURCE[0]}")
 read -p "Do you wish to configure a custom upstream CA for SPIRE? [yn] " -n 1 yn
@@ -50,36 +38,35 @@ case $yn in
             exit 
         fi 
         echo -e "\nGenerating custom CA for SPIRE";
-        cacfg $flags
+        cacfg
         ;;
     [Nn]* ) echo -e "\nUsing Quickstart CA for SPIRE"; exit;;
     * ) echo -e "\nPlease answer yes or no. Defaulting to Quickstart CA"; exit;;
 esac
 
-cmd='acert authorities create '${flags}''
-AUTH_FINGERPRINT=$($cmd)
+AUTH_FINGERPRINT=$(acert authorities create -n "$N" -o "$O" -c "$C" -s "$S" -l "$L" -u "$U" -e "$E" -a "$A" -p "$P")
 
 kubectl create secret generic server-ca \
   -n $NS \
   -o yaml \
   --dry-run=client \
-  --from-literal=root.crt="$(acert authorities export ${AUTH_FINGERPRINT} -t authority -f pem)" \
-  --from-literal=intermediate.crt="$(acert authorities export ${AUTH_FINGERPRINT} -t certificate -f pem)" \
-  --from-literal=intermediate.key="$(acert authorities export ${AUTH_FINGERPRINT} -t key -f pem)" > \
+  --from-literal=root.crt="$(acert authorities export $AUTH_FINGERPRINT -t authority -f pem)" \
+  --from-literal=intermediate.crt="$(acert authorities export $AUTH_FINGERPRINT -t certificate -f pem)" \
+  --from-literal=intermediate.key="$(acert authorities export $AUTH_FINGERPRINT -t key -f pem)" > \
   ../../spire/server/templates/server-ca-secret.yaml
 
-REGISTRAR_FINGERPRINT=$(acert authorities issue ${AUTH_FINGERPRINT} -n 'registrar.'${NS}'.svc')
+REGISTRAR_FINGERPRINT=$(acert authorities issue $AUTH_FINGERPRINT -n 'registrar.'$NS'.svc')
 
 kubectl create secret generic server-tls \
   -n $NS \
   -o yaml \
   --dry-run=client \
-  --from-literal=ca.crt="$(acert leaves export ${REGISTRAR_FINGERPRINT} -t authority -f pem)" \
-  --from-literal=registrar.${NS}.svc.crt="$(acert leaves export ${REGISTRAR_FINGERPRINT} -t certificate -f pem)" \
-  --from-literal=registrar.${NS}.svc.key="$(acert leaves export ${REGISTRAR_FINGERPRINT} -t key -f pem)" > \
+  --from-literal=ca.crt="$(acert leaves export $REGISTRAR_FINGERPRINT -t authority -f pem)" \
+  --from-literal=registrar.${NS}.svc.crt="$(acert leaves export $REGISTRAR_FINGERPRINT -t certificate -f pem)" \
+  --from-literal=registrar.${NS}.svc.key="$(acert leaves export $REGISTRAR_FINGERPRINT -t key -f pem)" > \
   ../../spire/server/templates/server-tls-secret.yaml
 
-CABUNDLE="$(acert leaves export ${REGISTRAR_FINGERPRINT} -t authority -f pem | base64)"
+CABUNDLE="$(acert leaves export $REGISTRAR_FINGERPRINT -t authority -f pem | base64)"
 
 sed -i "" "s/caBundle: .*/caBundle: $CABUNDLE/" ../../spire/server/templates/validatingwebhookconfiguration.yaml
 
